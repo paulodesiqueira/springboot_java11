37032fedda514f6094cc599751adcd53e8c8df6d
paulodesiqueira <paulodesiqueira@gmail.com>
2021-10-20 14:54:01 -0300

    Projet Created

---
+++ src/main/java/com/siqueyros/cursospring/CursoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.siqueyros.cursospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringApplication.class, args);
+	}
+
+}
